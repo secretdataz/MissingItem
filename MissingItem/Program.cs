2a278ac832831c732c5b66dda3701de5e146b37e
Jittapan Pluemsumran <foreverfk@stephack.com>
2016-04-26 17:27:08 +0700

    Initial code

--- MissingItem/Program.cs
+++ MissingItem/Program.cs
@@ -1,14 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 
 namespace MissingItem
 {
     class Program
     {
+        static string ServerDb;
+        static string ItemInfo;
+        static string OutFile;
         static void Main(string[] args)
         {
+            if(args.Length < 2)
+            {
+                Log("Not enough arguments. Defaulting target files to item_db.txt, ItemInfo.lua");
+                ServerDb = Path.Combine(Directory.GetCurrentDirectory(), "item_db.txt");
+                ItemInfo = Path.Combine(Directory.GetCurrentDirectory(), "ItemInfo.lua");
+            }else
+            {
+                ServerDb = Path.Combine(Directory.GetCurrentDirectory(), args[0]);
+                ItemInfo = Path.Combine(Directory.GetCurrentDirectory(), args[1]);
+            }
+            if(args.Length < 3)
+            {
+                Log("Output file not specified. Defaulting to MissingItems.txt");
+                OutFile = Path.Combine(Directory.GetCurrentDirectory(), "MissingItems.txt");
+            }else
+            {
+                OutFile = Path.Combine(Directory.GetCurrentDirectory(), args[2]);
+            }
+            Log("Starting program..");
+            new MissingItem(ServerDb, ItemInfo, OutFile).GenerateOutput();
         }
+
+        static void Log(string arg) { Console.WriteLine("[*] " + arg);  }
     }
 }
